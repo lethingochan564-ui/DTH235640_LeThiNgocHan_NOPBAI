d037cf25ce180b0fe43a5214823bd3603b0917b4
DTH235640_LeThiNgocHan <lethingochan564@gmail.com>
2025-09-04 20:18:14 +0700

    Delete Cau10.py

--- Cau10.py
+++
@@ -1,8 +0,0 @@
-h = [1, 3, 5, 3, 7, 9]
-max_width = max(h)
-for sao in h:
-    space = (max_width - sao) // 2
-    print(" " * space + "*" * sao)
-for _ in range(2):
-    space = (max_width - 3) // 2
-    print(" " * space + "*" * 3)
