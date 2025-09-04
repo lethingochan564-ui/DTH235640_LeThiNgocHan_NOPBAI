1f9c25e09f26c48e8410857bb9577737dc1c6eb3
DTH235640_LeThiNgocHan <lethingochan564@gmail.com>
2025-09-04 20:19:40 +0700

    Delete Cau7.py

--- Cau7.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if len(sys.argv) < 2:
-    print("Vui lòng nhập chuỗi cần in dưới dạng tham số dòng lệnh.")
-else:
-    # Nối tất cả tham số từ argv[1] trở đi thành chuỗi hoàn chỉnh
-    chuoi = ' '.join(sys.argv[1:])
-    print("Chuỗi đã nhập là:", chuoi)
